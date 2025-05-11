5029ec74b660a4500420520bc953b41c4d89d075
Srivatsa <40351995+devsrivatsa@users.noreply.github.com>
2025-05-11 10:24:47 -0400

    Create BestTimeToBuyAndSellStock2.ts

---
+++ leetcodeJS/greedy/BestTimeToBuyAndSellStock2.ts
@@ -0,0 +1,11 @@
+function maxProfit(prices: number[]): number {
+    let maxProfit = 0;
+    for (let i=1; i<prices.length; i++) {
+        if (prices[i] > prices[i-1]) {
+            maxProfit += (prices[i] - prices[i-1])
+        }
+    }
+    return maxProfit
+};
+
+//TC, SC: O(n)
